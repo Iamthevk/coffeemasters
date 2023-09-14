5084e16a0b133a2dbec60d4cb04c096431166cd7
Iamthevk <vijay.vuex@gmail.com>
2023-09-14 23:46:37 +0530

    routing updated for product page

--- services/Router.js
+++ services/Router.js
@@ -11,34 +11,52 @@ const Router = {
     //event handler for url changes
     window.addEventListener("popstate", (e) => {
       // console.log(e);
-      Router.go(e.state.path, false);
+      Router.go(e.state.route, false);
     });
 
     //check the initial url
     Router.go(location.pathname);
   },
   go(path, addToHistory = true) {
-    console.log(`Going to ${path}`);
-
+    // console.log(`Going to ${path}`);
     if (addToHistory) {
       history.pushState({ path }, "", path);
     }
-    let pageElement;
+    let pageElement = null;
     switch (path) {
       case "/":
         pageElement = document.createElement("menu-page");
         break;
       case "/order":
-        pageElement = document.createElement("h1");
+        pageElement = document.createElement("order-page");
+        break;
+      default:
+        if (path.startsWith("/product-")) {
+          pageElement = document.createElement("details-page");
+          pageElement.dataset.productId = path.substring(
+            path.lastIndexOf("-") + 1
+          );
+        }
         break;
     }
     if (pageElement) {
-      const main = document.querySelector("main");
-      main.innerHTML = "";
-      main.appendChild(pageElement);
-      window.scrollX = 0;
-      window.scrollY = 0;
+      let currentPage = document.querySelector("main").firstElementChild;
+      if (currentPage) {
+        let fadeOut = currentPage.animate([{ opacity: 1 }, { opacity: 0 }], {
+          duration: 200,
+        });
+        fadeOut.addEventListener("finish", () => {
+          currentPage.remove();
+          document.querySelector("main").appendChild(pageElement);
+          let fadeIn = pageElement.animate([{ opacity: 0 }, { opacity: 1 }], {
+            duration: 200,
+          });
+        });
+      } else {
+        document.querySelector("main").appendChild(pageElement);
+      }
     }
+    window.scrollX = 0;
   },
 };
 export default Router;
